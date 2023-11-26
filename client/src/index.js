624ac86c9e7b415c796ea75162ee1032516812d5
Shaik Saad <shaiksaadullahshareef@gmail.com>
2023-11-26 08:54:05 -0600

    change in cors

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // creating base URL using axios
 const axiosinstance = axios.create({
-  baseURL: process.env.CLIENT_URL || 'http://localhost:3000/api',
+  baseURL: process.env.REACT_APP_CLIENT_URL || 'http://localhost:3000/api',
   credentials: 'include'
 })
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use('/api',
         createProxyMiddleware({
-            target: process.env.BACKEND_API || 'http://localhost:4000',
+            target: process.env.REACT_APP_BACKEND_API || 'http://localhost:4000',
             changeOrigin: true,
             pathRewrite: { '^/api': '' }
         })
