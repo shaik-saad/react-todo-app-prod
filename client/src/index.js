8decf1bc76b7966fb39c339e9965f58d7dba87f9
Shaik Saad <shaiksaadullahshareef@gmail.com>
2023-11-26 08:09:34 -0600

    change in cors

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RequestProvider } from 'react-request-hook'
-import axios from 'axios';
-import {config} from 'dotenv'
-config()
+import axios from 'axios'; 
 
 // creating base URL using axios
 const axiosinstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.BACKEND_API || 'http://localhost:3000/api',
   credentials: 'include'
 })
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-    app.use('/api',
-        createProxyMiddleware({
-            target: process.env.BACKEND_API || 'http://localhost:4000',
-            changeOrigin: true,
-            pathRewrite: { '^/api': '' }
-        })
-    );
-}
+// module.exports = function (app) {
+//     app.use('/api',
+//         createProxyMiddleware({
+//             target: process.env.BACKEND_API || 'http://localhost:4000',
+//             changeOrigin: true,
+//             pathRewrite: { '^/api': '' }
+//         })
+//     );
+// }
